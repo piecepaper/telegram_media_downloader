c0eb16a82a2674fc70bacd33ce7bc30f93232b2f
tom <314725332@qq.com>
2026-07-29 10:14:43 +0800

    fix: 隐藏文件名中的敏感信息

--- windows_telegram_make_placeholder.py
+++ windows_telegram_make_placeholder.py
@@ -44,7 +44,7 @@ def main():
         # 1.隐藏文件名名字 2.名字太长了
         file_name, file_suffix = rel_path.rsplit(".", 1)
         file_name = file_name.split('-', 1)[0]
-        show_path = f"{file_name}- xxxx.{file_suffix}"
+        show_path = f"{file_name}- ****.{file_suffix}"
         line.update(f"[{i+1:>{width}}/{total}] 创建稀疏文件 {show_path}")
 
         # 创建稀疏文件
--- windows_telegram_placeholder.py
+++ windows_telegram_placeholder.py
@@ -138,7 +138,7 @@ def run():
         # 1.隐藏文件名名字 2.名字太长了
         file_name, file_suffix = rel_path.rsplit(".", 1)
         file_name = file_name.split('-', 1)[0]
-        show_path = f"{file_name}- xxxx.{file_suffix}"
+        show_path = f"{file_name}- ****.{file_suffix}"
         line.update(f"[{i+1:>{width}}/{total}] 移动文件 {show_path}")
 
         # 使用 shutil.move 跨磁盘也可以处理
